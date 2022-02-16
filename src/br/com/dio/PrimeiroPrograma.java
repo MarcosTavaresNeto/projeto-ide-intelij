f626f098483d6a114eb1b8c4c0d74938df89e97e
Marcos <marvos.tavares@gmail.com>
2022-02-15 21:40:13 -0300

    Make Class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -2,6 +2,8 @@ package br.com.dio;
 
 import br.com.dio.model.Gato;
 
+import java.util.Objects;
+
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         /*int a=5;
@@ -9,6 +11,43 @@ public class PrimeiroPrograma {
         System.out.println("Soma = "+ (a+b));*/
         Gato cat = new Gato("Meaw", "Branco", 10);
         System.out.println(cat);
+        Livro book = new Livro("A volta dos que não foram", 123);
+        System.out.println(book);
 
     }
 }
+
+class Livro {
+    private String nome;
+    private int numPag;
+
+    public Livro(String nome, int numPag) {
+        this.nome = nome;
+        this.numPag = numPag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNumPag() {
+        return numPag;
+    }
+
+    public void setNumPag(int numPag) {
+        this.numPag = numPag;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPag=" + numPag +
+                '}';
+    }
+}
+
